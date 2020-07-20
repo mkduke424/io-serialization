7ae96087d642a1e9804d911198ced7d70dad6708
miles <dkmk1149@gmail.com>
2020-07-19 23:40:42 -0400

    added the ability to save/resume game progress with serialization

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,4 +1,5 @@
 puts "--Hangman Game Started--"
+require 'yaml'
 
 # Pulls a random word from the 5desk.txt(dictionary of words) file that is between 5 and 12 charachters long 
 def random_word
@@ -13,7 +14,7 @@ def random_word
 end
 
 class HangMan 
- attr_accessor :word, :status, :incorrect_guesses
+ attr_accessor :word, :status, :incorrect_guesses, :arr_word
   def initialize(word) 
     @word = word
     @arr_word = word.split("")
@@ -25,10 +26,40 @@ class HangMan
     @incorrect_guesses = []
   end
 
-  def play_round
-    puts "Guess a letter: "
-    guessed_letter = gets.chomp.downcase
+  def save_file
+    save_line = YAML.dump({
+      :word => @word,
+      :arr_word => @arr_word,
+      :status => @status,
+      :incorrect_guesses => @incorrect_guesses
+    })
 
+    save_file = File.open("save.txt","w")
+    save_file.puts save_line
+    save_file.close
+    puts "we have now saved your game"
+  end
+
+  def load_file
+    saved_file = File.open("save.txt", "r")
+    saved_line = saved_file.read
+    instance_variables = YAML.load saved_line
+    @word = instance_variables[:word]
+    @arr_word = instance_variables[:arr_word]
+    @status = instance_variables[:status]
+    @incorrect_guesses = instance_variables[:incorrect_guesses]
+    puts "we have loaded your saved game"
+    p @status
+    p @incorrect_guesses
+  end
+
+  def play_round
+    puts "Guess a letter or press 1 to save your progress: "
+    guessed_letter = gets.chomp.strip.downcase
+    if guessed_letter == "1"
+      save_file
+      exit(true)
+    end
     # if the guessed letter isn't found in the chosen word then add it to incorrect guesses array
     @incorrect_guesses.push(guessed_letter) unless @arr_word.include? guessed_letter
 
@@ -45,12 +76,19 @@ class HangMan
     p "You have #{ 6 - incorrect_guesses.length} incorrect guesses left"
   end
 end
-
+puts "Do you want to lead in your previously saved game? - Y for yes or press any other key for no"
+response = gets.chomp.downcase
+if response == "y"
 hangman = HangMan.new(random_word)
+hangman.load_file
+else
+  hangman = HangMan.new(random_word)
+  p hangman.status
+end
 
 until hangman.incorrect_guesses.length == 6
   hangman.play_round
-
+  
   # The winner is announced when the status doesn't contain "_" characters 
   unless hangman.status.include? '_'
     puts 'Congrats you have won!'
